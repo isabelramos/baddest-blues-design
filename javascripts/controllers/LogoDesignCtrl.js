d11154bc6105d80071a24da714f932af81bb7e64
Isabel Ramos <iramos1226@gmail.com>
2017-10-06 22:07:06 -0500

    carousel basic styling

--- javascripts/controllers/LogoDesignCtrl.js
+++ javascripts/controllers/LogoDesignCtrl.js
@@ -1,3 +1,12 @@
-app.controller("LogoDesignCtrl", function() {
+app.controller("LogoDesignCtrl", function($http, $scope) {
+
+	$scope.logos = [];
+	console.log($scope.logos);
+
+	$http.get('/database/seed.json')
+	   .then(function(results) {
+	   		$scope.logos = results.data.logos;
+	   		console.log($scope.logos);
+	});
 
 });
